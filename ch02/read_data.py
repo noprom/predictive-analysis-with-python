6a1c18741033ea843dcdad6c3edf4834a93d6f3c
noprom <tyee.noprom@qq.com>
2016-04-17 23:25:34 +0800

    read with open

--- ch02/read_data.py
+++ ch02/read_data.py
@@ -11,4 +11,11 @@ path = '../data/ch02'                   # 从csv文件读取数据
 filename = 'titanic3.csv'
 fullpath = os.path.join(path,filename)
 data = pd.read_csv(fullpath)            # 读取数据
-print(data.columns.values)              # 打印表头
+# print(data.columns.values)              # 打印表头
+
+# 使用 open 函数来打开
+data = open('../data/ch02/Customer Churn Model.txt', 'r')
+cols = data.next().strip().split(',')
+no_cols = len(cols)
+print(cols)
+print(no_cols)
