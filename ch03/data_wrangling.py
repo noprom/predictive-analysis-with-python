db04917e6a5b31bd2c59970b24cac49047ac75d9
noprom <tyee.noprom@qq.com>
2016-04-18 21:11:45 +0800

    add another way of choosing the columns

--- ch03/data_wrangling.py
+++ ch03/data_wrangling.py
@@ -12,6 +12,11 @@ account_length = data['Account Length']  # 选择一列
 print account_length.head()
 print type(account_length)
 
-subdata = data[['Account Length', 'VMail Message', 'Day Calls']]    # 选择多列
+subdata = data[['Account Length', 'VMail Message', 'Day Calls']]  # 选择多列
 print subdata.head()
 print type(subdata)
+
+# 另外一种选择多个子列的方式
+wanted_columns = ['Account Length', 'VMail Message', 'Day Calls']
+subdata = data[wanted_columns]
+print subdata.head()
