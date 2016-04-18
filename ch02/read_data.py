979a516973b6f4c546f5c59a97a82445641acb9b
noprom <tyee.noprom@qq.com>
2016-04-18 19:57:47 +0800

    dummy_sex

--- ch02/read_data.py
+++ ch02/read_data.py
@@ -7,6 +7,14 @@
 '''
 import pandas as pd
 import os
+# 使用 open 函数来打开
+data = open('../data/ch02/Customer Churn Model.txt', 'r')
+cols = data.next().strip().split(',')
+no_cols = len(cols)
+print(cols)
+print(no_cols)
+
+# 使用read_csv读取文件内容
 path = '../data/ch02'                   # 从csv文件读取数据
 filename = 'titanic3.csv'
 fullpath = os.path.join(path,filename)
@@ -22,9 +30,6 @@ print data['age'].fillna(data['age'].mean()) # 用平均数替换NaN
 print data['age'].fillna(method='ffill') # 使用前面最近的来替换
 print data['age'].fillna(method='backfill') # 使用后面最近的来替换
 
-# 使用 open 函数来打开
-data = open('../data/ch02/Customer Churn Model.txt', 'r')
-cols = data.next().strip().split(',')
-no_cols = len(cols)
-print(cols)
-print(no_cols)
+# 将性别分别转化为0和1
+dummy_sex = pd.get_dummies(data=data['sex'], prefix='sex')
+print dummy_sex
