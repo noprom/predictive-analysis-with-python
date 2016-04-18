2f89e1c38506420630405549bcaed03f4777a049
noprom <tyee.noprom@qq.com>
2016-04-18 19:28:07 +0800

    dtypes the data

--- ch02/read_data.py
+++ ch02/read_data.py
@@ -11,8 +11,9 @@ path = '../data/ch02'                   # 从csv文件读取数据
 filename = 'titanic3.csv'
 fullpath = os.path.join(path,filename)
 data = pd.read_csv(fullpath)            # 读取数据
-print data.describe()
-print data.columns.values              # 打印表头
+print data.describe()                   # 描述数据
+print data.columns.values               # 打印表头
+print data.dtypes                       # 打印数据的类型
 
 # 使用 open 函数来打开
 data = open('../data/ch02/Customer Churn Model.txt', 'r')
