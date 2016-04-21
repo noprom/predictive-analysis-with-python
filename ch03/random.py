9be402a50484782ad64904a10fc68d9e12bc18f4
noprom <tyee.noprom@qq.com>
2016-04-21 18:40:39 +0800

    randint_range

--- ch03/random.py
+++ ch03/random.py
@@ -14,3 +14,17 @@ print random
 # 生成一个0到1的随机小数
 random = np.random.random()
 print random
+
+
+def randint_range(n, a, b):
+    '''
+    生成n个a~b之间的随机数
+    :param n: 随机数个数
+    :param a: 最小值
+    :param b: 最大值
+    :return: 随机数list
+    '''
+    x = []
+    for i in range(n):
+        x.append(np.random.randint(a, b))
+    return x
