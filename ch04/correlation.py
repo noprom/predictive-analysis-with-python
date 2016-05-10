006c639b06bb216f2fc9c6af2d2896fd49075e69
noprom <tyee.noprom@qq.com>
2016-05-10 21:31:45 +0800

    correlation func calculated

--- ch04/correlation.py
+++ ch04/correlation.py
@@ -11,17 +11,8 @@ import numpy as np
 advert = pd.read_csv("../data/ch05/Advertising.csv")
 print advert.head()
 
-# find out the correlation between the advertisement costs on TV and the resultant sales.
-advert['corrn'] = (advert['TV'] - np.mean(advert['TV'])) * (advert['Sales'] - np.mean(advert['Sales']))
-advert['corrd1'] = (advert['TV'] - np.mean(advert['TV'])) ** 2
-advert['corrd2'] = (advert['Sales'] - np.mean(advert['Sales'])) ** 2
-corrcoeffn = advert.sum()['corrn']
-corrcoeffd1 = advert.sum()['corrd1']
-corrcoeffd2 = advert.sum()['corrd2']
-corrcoeffd = np.sqrt(corrcoeffd1 * corrcoeffd2)
-corrcoeff = corrcoeffn / corrcoeffd
-print corrcoeff
 
+# find out the correlation between the advertisement costs on TV and the resultant sales.
 
 # Calculate correlation func
 def corrcoeff(df, var1, var2):
@@ -34,3 +25,7 @@ def corrcoeff(df, var1, var2):
     corrcoeffd = np.sqrt(corrcoeffd1 * corrcoeffd2)
     corrcoeff = corrcoeffn / corrcoeffd
     return corrcoeff
+
+
+corrcoeff_val = corrcoeff(advert, 'TV', 'Sales')
+print corrcoeff_val
